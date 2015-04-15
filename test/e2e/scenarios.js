62a3746feaadc2fa0d41d872b95b13bb6dea3803
live4code <live4code@gmail.com>
2015-04-15 18:58:10 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,40 +1,12 @@
 'use strict';
 
-/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
-
 describe('PhoneCat App', function() {
-
-  it('should redirect index.html to index.html#/phones', function() {
-    browser.get('app/index.html');
-    browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/phones');
-      });
-  });
-
-
-  describe('Phone list view', function() {
-
-    beforeEach(function() {
-      browser.get('app/index.html#/phones');
-    });
-
-    it('should render phone specific links', function() {
-      var query = element(by.model('query'));
-      query.sendKeys('nexus');
-      element.all(by.css('.phones li a')).first().click();
-      browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/phones/nexus-s');
-      });
-    });
-  });
-
 
   describe('Phone detail view', function() {
 
     beforeEach(function() {
       browser.get('app/index.html#/phones/nexus-s');
     });
-
 
     it('should display nexus-s page', function() {
       expect(element(by.binding('phone.name')).getText()).toBe('Nexus S');
