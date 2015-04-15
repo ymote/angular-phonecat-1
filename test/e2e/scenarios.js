edf390cb696f9d4bc04090960858fa63e1134840
live4code <live4code@gmail.com>
2015-04-15 08:33:25 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,15 +1,11 @@
 'use strict';
 
-/* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
-
 describe('PhoneCat App', function() {
-
   describe('Phone list view', function() {
 
     beforeEach(function() {
       browser.get('app/index.html');
     });
-
 
     it('should filter the phone list as user types into the search box', function() {
 
@@ -18,42 +14,10 @@ describe('PhoneCat App', function() {
 
       expect(phoneList.count()).toBe(20);
 
-      query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
-
-      query.clear();
       query.sendKeys('motorola');
       //how many phones are on page now
       expect(phoneList.count()).toBe(0);
     });
-
-
-    it('should be possible to control phone order via the drop down select box', function() {
-
-      var phoneNameColumn = element.all(by.repeater('phone in phones').column('{{phone.name}}'));
-      var query = element(by.model('query'));
-
-      function getNames() {
-        return phoneNameColumn.map(function(elm) {
-          return elm.getText();
-        });
-      }
-
-      query.sendKeys('tablet'); //let's narrow the dataset to make the test assertions shorter
-
-      expect(getNames()).toEqual([
-        "Motorola XOOM\u2122 with Wi-Fi",
-        "MOTOROLA XOOM\u2122"
-      ]);
-
-      element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
-
-      expect(getNames()).toEqual([
-        "MOTOROLA XOOM\u2122",
-        "Motorola XOOM\u2122 with Wi-Fi"
-      ]);
-    });
-
 
     it('should render phone specific links', function() {
       var query = element(by.model('query'));
