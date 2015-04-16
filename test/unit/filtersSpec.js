1b7fcef1685948dba95190fea442fe7e0118813e
live4code <live4code@gmail.com>
2015-04-16 08:52:30 +0000

    commit exercise content by Live4Code

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -6,14 +6,13 @@ describe('filter', function() {
 
   beforeEach(module('phonecatFilters'));
 
-
   describe('checkmark', function() {
 
     it('should convert boolean values to unicode checkmark or cross',
         inject(function(checkmarkFilter) {
       expect(checkmarkFilter(true)).toBe('\u2713');
-      //加上对false的判断，符号的内容可以从filters.js中获得
-
+      //add expect test on the false case, you can get the unicode characters for false in filters.js
+      
     }));
   });
 });
