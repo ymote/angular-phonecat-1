346232c2fe056cb37caaa5db397ff2eb5063fe64
live4code <live4code@gmail.com>
2015-04-16 07:36:13 +0000

    commit exercise content by Live4Code

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -5,34 +5,6 @@ describe('PhoneCat controllers', function() {
 
   beforeEach(module('phonecatApp'));
 
-  describe('PhoneListCtrl', function(){
-    var scope, ctrl, $httpBackend;
-
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/phones.json').
-          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
-
-      scope = $rootScope.$new();
-      ctrl = $controller('PhoneListCtrl', {$scope: scope});
-    }));
-
-
-    it('should create "phones" model with 2 phones fetched from xhr', function() {
-      expect(scope.phones).toBeUndefined();
-      $httpBackend.flush();
-
-      expect(scope.phones).toEqual([{name: 'Nexus S'},
-                                   {name: 'Motorola DROID'}]);
-    });
-
-
-    it('should set the default value of orderProp model', function() {
-      expect(scope.orderProp).toBe('age');
-    });
-  });
-
-
   describe('PhoneDetailCtrl', function(){
     var scope, $httpBackend, ctrl;
 
