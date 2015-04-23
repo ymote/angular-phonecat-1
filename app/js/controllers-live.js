e72d0ef3c892f89826b396f199c490d0df106fd9
live4code <live4code@gmail.com>
2015-04-23 02:37:44 +0000

    commit exercise content by Live4Code

--- app/js/controllers-live.js
+++ app/js/controllers-live.js
@@ -2,16 +2,16 @@
 
 /* Controllers */
 
+var phonecatApp = angular.module('phonecatApp', []);
 
-
-
-  
-   
-     
-   
+phonecatApp.controller('PhoneListCtrl', function($scope) {
+  $scope.phones = [
+   {'name': 'Nexus S','snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi','snippet': 'The Next, Next Generation tablet.'}, 
+   {'name': 'MO
+  ];  
     
-    
-     
+});     
   
 
   
