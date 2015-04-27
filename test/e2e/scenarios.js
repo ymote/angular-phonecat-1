1a61b8ecf673f84ed8599f4b5cbdb4d5437b81b9
live4code <live4code@gmail.com>
2015-04-27 19:35:08 +0000

    commit exercise content by Live4Code

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -10,23 +10,15 @@ describe('Test PhoneCat App', function() {
       browser.get('app/index.html');
     });
 
-
     it('should filter the phone list as user types into the search box', function() {
 
       var phoneList = element.all(by.repeater('phone in phones'));
       var query = element(by.model('query'));
 
-      expect(phoneList.count()).toBe(3);
-
-      query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
-
-      query.clear();
       query.sendKeys('motorola');
       //how many phones in phoneList now?
       expect(phoneList.count()).toBe(0);
     });
-
 
     it('should be possible to control phone order via the drop down select box', function() {
 
